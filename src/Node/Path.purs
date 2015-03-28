7c123b17d9fbdd83d9edfe2d77a0499bdee0a856
Gary Burgess <gary.burgess@gmail.com>
2015-03-28 16:34:44 +0000

    Rename `join` and update docs for rendering

--- src/Node/Path.purs
+++ src/Node/Path.purs
@@ -1,29 +1,21 @@
 module Node.Path where
 
--- |
--- Type for strings representing file paths.
---
+-- | Type for strings representing file paths.
 type FilePath = String
 
--- |
--- Normalize a string path, taking care of `..` and `.`, duplicated slashes,
--- etc. If the path contains a trailing slash it is preserved. On Windows
--- backslashes are used.
---
+-- | Normalize a string path, taking care of `..` and `.`, duplicated slashes,
+-- | etc. If the path contains a trailing slash it is preserved. On Windows
+-- | backslashes are used.
 foreign import normalize
   "var normalize = require('path').normalize;" :: FilePath -> FilePath
 
--- |
--- Joins two path segments together and normalizes the resulting path.
---
-foreign import join
-  "var join = function (segments) { \
+-- | Concatenates multiple path segments together and normalizes the resulting path.
+foreign import concat
+  "var concat = function (segments) { \
   \  return require('path').join.apply(this, segments); \
   \}" :: [FilePath] -> FilePath
 
--- |
--- Resolves `to` to an absolute path ([from...], to).
---
+-- | Resolves `to` to an absolute path ([from...], to).
 foreign import resolve
   "var resolve = function (from) { \
   \  return function (to) { \
@@ -31,9 +23,7 @@ foreign import resolve
   \  }; \
   \};" :: [FilePath] -> FilePath -> FilePath
 
--- |
--- Solve the relative path from `from` to `to`.
---
+-- | Solve the relative path from `from` to `to`.
 foreign import relative
   "var relative = function (from) { \
   \  return function (to) { \
@@ -41,25 +31,19 @@ foreign import relative
   \  }; \
   \}" :: FilePath -> FilePath -> FilePath
 
--- |
--- Return the directory name of a path.
---
+-- | Return the directory name of a path.
 foreign import dirname
   "var dirname = function (path) { \
   \  var p = require('path'); \
   \  return p.normalize(p.dirname(path)); \
   \}" :: FilePath -> FilePath
 
--- |
--- Return the last portion of a path.
---
+-- | Return the last portion of a path.
 foreign import basename
   "var basename = require('path').basename;" :: FilePath -> FilePath
 
--- |
--- Return the last portion of a path, also dropping a specific file extension
--- if it matches the end of the name.
---
+-- | Return the last portion of a path, also dropping a specific file extension
+-- | if it matches the end of the name.
 foreign import basenameWithoutExt
   "var basenameWithoutExt = function (path) { \
   \  return function (ext) { \
@@ -67,22 +51,16 @@ foreign import basenameWithoutExt
   \  }; \
   \}" :: FilePath -> FilePath -> FilePath
 
--- |
--- Return the extension of the path, from the last `.` to end of string in the
--- last portion of the path. If there is no `.` in the last portion of the path
--- or the first character of it is `.`, then it returns an empty string.
---
+-- | Return the extension of the path, from the last `.` to end of string in the
+-- | last portion of the path. If there is no `.` in the last portion of the
+-- | path or the first character of it is `.`, then it returns an empty string.
 foreign import extname
   "var extname = require('path').extname;" :: FilePath -> FilePath
 
--- |
--- The platform-specific file separator. `\\` or `/`.
---
+-- | The platform-specific file separator. `\\` or `/`.
 foreign import sep
   "var sep = require('path').sep;" :: String
 
--- |
--- The platform-specific path delimiter, `;` or `:`.
---
+-- | The platform-specific path delimiter, `;` or `:`.
 foreign import delimiter
   "var delimiter = require('path').delimiter;" :: String
