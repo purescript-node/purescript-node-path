fb8e9e78de9df739f7ff32d01c93ccd1e7f89a35
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2022-02-12 11:26:54 +0000

    Arrow/lest transformation

--- src/Node/Path.js
+++ src/Node/Path.js
@@ -1,40 +1,34 @@
 "use strict";
 
 import path from "path";
-export var normalize = path.normalize;
+export const normalize = path.normalize;
 
 export function concat(segments) {
   return path.join.apply(this, segments);
 }
 
 export function resolve(from) {
-  return function (to) {
-    return function () {
-      return path.resolve.apply(this, from.concat([to]));
-    };
-  };
+  return to => (function() {
+    return path.resolve.apply(this, from.concat([to]));
+  });
 }
 
 export function relative(from) {
-  return function (to) {
-    return path.relative(from, to);
-  };
+  return to => path.relative(from, to);
 }
 
 export function dirname(p) {
   return path.normalize(path.dirname(p));
 }
 
-export var basename = path.basename;
+export const basename = path.basename;
 
 export function basenameWithoutExt(p) {
-  return function (ext) {
-    return path.basename(p, ext);
-  };
+  return ext => path.basename(p, ext);
 }
 
-export var extname = path.extname;
-export var sep = path.sep;
-export var delimiter = path.delimiter;
-export var parse = path.parse;
-export var isAbsolute = path.isAbsolute;
+export const extname = path.extname;
+export const sep = path.sep;
+export const delimiter = path.delimiter;
+export const parse = path.parse;
+export const isAbsolute = path.isAbsolute;
